ba3db0b637d05cae6532ce55eeb8a61a73841974
Niraj Kumar <nk659192@gmail.com>
2024-07-21 13:52:19 +0530

    complete frontenrd

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const navBottom = document.querySelector("nav button");
+    navBottom.addEventListener("click", () => {
+        alert("Thank you for reaching out to us! We will get back to you shortly.");
+    });
+});
